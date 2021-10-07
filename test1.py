97644064c420b9d89fffc7e7e713ac7503027858
ShlomyR <91196585+ShlomyR@users.noreply.github.com>
2021-10-07 14:02:01 +0300

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,12 @@
+ieierjiehr 
+
+'pri39ur[r 3or3
+R3r 3o0r39ri3oir3 
+ 3ioru3rpo3    3iri3ru3 3o r3o[r 
+                               3[
+                                  3]]
+  3rui93ur 33
+   oi3rjoi3 ru2tuqur9wu 0ru urr ur 
+    r 93ur 
+    w ur9
+    wqu 3 ur9u r93ur
